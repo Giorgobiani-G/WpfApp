4715f871df3c15251d5ce85eb5b6bd65b1b9582d
Giorgi Giorgobiani <giorgobiani086@gmail.com>
2020-05-24 20:40:57 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.econtact;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+
+    public partial class MainWindow : Window
+    {
+
+        ContactClass contact = new ContactClass();
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Select();
+
+        }
+
+
+        // Method to Select data from Database
+        public void Select()
+        {
+            //1.Connect to database
+
+            string myconstring = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            
+            SqlConnection sqlConnection = new SqlConnection(myconstring);
+
+
+            try
+            {
+                
+                //2. create sql querry
+
+                string sql = "Select * from Wpf_Contacts order by ContactId Asc";
+                
+               
+                //3.Create Cmd using sqlConnection and sql
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                
+                //4.Create Sqldataadapter using sqlCommand
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+                
+                DataTable dt = new DataTable("Wpf_Contacts");
+                sqlConnection.Open();
+                adapter.Fill(dt);
+                txtboxdtgrid.ItemsSource = dt.DefaultView;
+
+
+            }
+
+
+
+
+
+
+
+
+            catch (Exception ex)
+            {
+
+
+
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+
+
+
+
+
+
+        }
+
+         //add data to database
+        private void Addbutton_Click(object sender, RoutedEventArgs e)
+        {
+            //Get data from input fields
+            contact.FirstName = txtboxFirstName.Text;
+            contact.LastName = txtboxLastName.Text;
+            contact.ContactNo = txtboxContactNo.Text;
+            contact.Address = txtboxAddress.Text;
+            contact.Gender = txtboxGender.SelectionBoxItem.ToString();
+
+
+
+
+
+
+            //Inserting data into database via method we greared
+            bool success = contact.Insert(contact);
+            if (success == true)
+            {
+                MessageBox.Show("New Contact inserted succesfully");
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Failed to new Contact, Try Again");
+
+            }
+
+
+            Select();
+
+        }
+
+        //Get Data from Datagrid to textboxes by mouse click
+        
+        private void Txtboxdtgrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid grid = (DataGrid)sender;
+            DataRowView rowselected = grid.SelectedItem as DataRowView;
+            if (rowselected != null) { 
+            txtboxcontactid.Text = rowselected[0].ToString();
+            txtboxFirstName.Text = rowselected[1].ToString();
+            txtboxLastName.Text = rowselected[2].ToString();
+            txtboxContactNo.Text = rowselected[3].ToString();
+            txtboxAddress.Text = rowselected[4].ToString();
+            txtboxGender.Text = rowselected[5].ToString();
+            }
+           
+        }
+       
+        //Update data to database
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            contact.ContactId = int.Parse(txtboxcontactid.Text);
+            contact.FirstName = txtboxFirstName.Text;
+            contact.LastName = txtboxLastName.Text;
+            contact.ContactNo = txtboxContactNo.Text;
+            contact.Address = txtboxAddress.Text;
+            contact.Gender = txtboxGender.SelectionBoxItem.ToString();
+
+
+            bool success = contact.Update(contact);
+            if (success == true)
+            {
+                MessageBox.Show("New Contact updated succesfully");
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Failed to update, Try Again");
+
+            }
+
+            Select();
+        }
+
+        //create Clear method
+         public void Clear() 
+
+        {
+            txtboxcontactid.Text = "";
+            txtboxFirstName.Text = "";
+            txtboxLastName.Text = "";
+            txtboxContactNo.Text = "";
+            txtboxAddress.Text = "";
+            txtboxGender.Text = "";
+        }
+        
+        //Clear textbox data
+        private void Clearbutton_Click(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+        
+        //Delete data from database
+        private void Deletebutton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            
+
+             
+            contact.ContactId = int.Parse(txtboxcontactid.Text);
+            bool success = contact.Delete(contact);
+            if (success == true)
+            {
+                MessageBox.Show("Contact Sucessfully Deleted");
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Failed to delete Contact");
+            }
+            
+            Select();
+            
+
+        }
+
+        //make searchox usable
+
+        string myconstring = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
+        private void Searchbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string keyword = searchbox.Text;
+            SqlConnection sqlConnection = new SqlConnection(myconstring);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("Select * from Wpf_Contacts where FirstName Like + '%"+keyword+ "%' or LastName Like + '%" + keyword + "%' or ContactNo Like + '%" + keyword + "%' or Address Like + '%" + keyword + "%' or Gender Like + '%" + keyword + "%' ", sqlConnection);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            txtboxdtgrid.ItemsSource = dt.DefaultView;
+        }
+
+        
+    }
+    
+}
---
+++ WpfApp1/econtact/ContactClass.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.econtact
+{
+    //Getter Setter Properties
+    //Acts as Data Carrier in  our Application
+    class ContactClass
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+        static readonly string myconstring = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        
+        
+
+        
+
+        //insert data into dababase
+
+        public bool Insert (ContactClass c)
+        {
+            //creating default value typa and set its value to false
+            bool issucess = false;
+
+            //Connect to database
+            SqlConnection sqlConnection = new SqlConnection(myconstring);
+            try
+            {
+                //create querry to insert data 
+                string sql = "INSERT INTO Wpf_Contacts (FirstName, LastName,ContactNo,Address,Gender) VALUES(@FirstName, @LastName, @ContactNo, @Address, @Gender)";
+                //Create sqlcommand using sqlconnection and sql
+                SqlCommand cmd = new SqlCommand(sql, sqlConnection);
+
+                //Create Parameters to add data
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+                cmd.Parameters.AddWithValue("@Address", c.Address);
+                cmd.Parameters.AddWithValue("@Gender", c.Gender);
+
+                //open connection
+                sqlConnection.Open();
+                int rows = cmd.ExecuteNonQuery();
+                //if the query runs succesfully rows will be greater than zero
+                if (rows > 0)
+                {
+                    issucess = true;
+                }
+                else
+                {
+                    issucess = false;
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+            return issucess;
+
+
+
+
+        }
+
+        //Method to update data into database
+        public bool Update(ContactClass c)
+        {
+            //creating default value typa and set its value to false
+            bool issucess = false;
+            SqlConnection con = new SqlConnection(myconstring);
+            try
+            {
+                
+                //create querry
+                string str = "Update Wpf_Contacts Set FirstName = @FirstName,LastName= @LastName, ContactNo= @ContactNo,Address= @Address,Gender=@Gender Where ContactId= @ContactId";
+                //Create comand
+                SqlCommand sqlCommand = new SqlCommand(str, con);
+                //Crete Parameters to Add value
+                sqlCommand.Parameters.AddWithValue("@ContactId", c.ContactId);
+                sqlCommand.Parameters.AddWithValue("@FirstName", c.FirstName);
+                sqlCommand.Parameters.AddWithValue("@LastName", c.LastName);
+                sqlCommand.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+                sqlCommand.Parameters.AddWithValue("@Address", c.Address);
+                sqlCommand.Parameters.AddWithValue("@Gender", c.Gender);
+                //open connection
+                con.Open();
+                int rows = sqlCommand.ExecuteNonQuery();
+                //if the rows querry runs succesfully rows number should be greager than zero
+
+                if (rows > 0)
+                {
+                    issucess = true;
+                }
+                else
+                {
+                    issucess = false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            finally
+            {
+                con.Close();
+             }
+
+            return issucess;
+        }
+
+        //Method to delete data into database 
+
+        public bool Delete(ContactClass c)
+        {
+            bool issucess = false;
+            SqlConnection sqlConnection = new SqlConnection(myconstring);
+
+            try
+            {
+                string del = "Delete From Wpf_Contacts Where ContactId = @ContactId";
+
+                SqlCommand cmd = new SqlCommand(del, sqlConnection);
+                cmd.Parameters.AddWithValue("@ContactId", c.ContactId);
+                sqlConnection.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    issucess = true;
+                }
+                else
+                {
+                    issucess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            return issucess;
+
+            
+        }
+
+        
+        
+    }
+    
+}
